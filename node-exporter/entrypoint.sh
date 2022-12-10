0f4e9576691f79d1c0827bbbc4b79d1365f7a4a6
Jeff Wong <awole20@gmail.com>
2022-12-10 11:49:32 -0800

    add node exporter. fix volumes for prom

---
+++ node-exporter/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+cd node_exporter-*.*-amd64
+./node_exporter
