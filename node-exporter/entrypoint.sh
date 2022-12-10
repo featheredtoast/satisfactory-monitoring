c2d8bd0ca84bd9fd52ab571b26c2610b2d3e0fdc
Jeff Wong <awole20@gmail.com>
2022-12-10 12:01:10 -0800

    add working things.

--- node-exporter/entrypoint.sh
+++ node-exporter/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+apt-get update
+apt-get install -y wget tar
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
 cd node_exporter-*.*-amd64
 ./node_exporter
