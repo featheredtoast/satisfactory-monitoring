10ca3f26b00dddc8648feeed620bccf99f5b6364
Jeff Wong <awole20@gmail.com>
2022-12-20 07:58:51 -0800

    adding some initial stuff to get map working

--- frmcompanion/init.sh
+++ frmcompanion/init.sh
@@ -2,4 +2,13 @@
 
 test ! -e Companion && /usr/bin/git clone --depth 1 -b multiarch https://github.com/featheredtoast/FicsitRemoteMonitoringCompanion.git .;
 cd Companion;
-/usr/local/go/bin/go run main.go -hostname fakeserver -port 8081
+mkdir -p ./bin/map
+test ! -e bin/companion && /usr/local/go/bin/go build main.go -o bin/companion
+apt update
+apt install -y nodejs npm
+cd ../map
+npm install
+npm run compile
+cp -R index.html map-16k.png vendor/ img/ js/ ../Companion/bin/map
+cd ../Companion/bin
+./companion -hostname fakeserver -port 8081
