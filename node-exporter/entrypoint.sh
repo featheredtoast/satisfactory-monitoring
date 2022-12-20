375815a2aab7b7fa0b064d3e60c213fe7135a5f6
Jeff Wong <awole20@gmail.com>
2022-12-20 10:53:28 -0800

    remove entrypoint on node exporter

--- node-exporter/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y wget tar
-wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-cd node_exporter-*.*-amd64
-./node_exporter
