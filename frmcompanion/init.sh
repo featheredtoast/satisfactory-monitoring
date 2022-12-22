df1d3cf8e7e0c73eaa3c74c1adf0b8129f3d0d27
Jeff Wong <awole20@gmail.com>
2022-12-21 18:56:02 -0800

    easier default stats.
    Also add a way to disable map install for faster boot times

--- frmcompanion/init.sh
+++ frmcompanion/init.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-test ! -e Companion && /usr/bin/git clone --depth 1 -b multiarch https://github.com/featheredtoast/FicsitRemoteMonitoringCompanion.git .
+test ! -e Companion && /usr/bin/git clone --depth 1 -b add-vehicle-stats https://github.com/featheredtoast/FicsitRemoteMonitoringCompanion.git .
 cd Companion
 mkdir -p ./bin/map
 test ! -e ./bin/companion && /usr/local/go/bin/go build -o bin/companion main.go
-test ! -e /usr/bin/npm && apt update && apt install -y nodejs npm
-cd ../map
-npm install
-npm run compile
-cp -R index.html map-16k.png vendor/ img/ js/ ../Companion/bin/map
+if [ -z "$INSTALL_MAP"]
+then
+    echo "Skipping map install. Set INSTALL_MAP var to true to enable."
+else
+    test ! -e /usr/bin/npm && apt update && apt install -y nodejs npm
+    cd ../map
+    npm install
+    npm run compile
+    cp -R index.html map-16k.png vendor/ img/ js/ ../Companion/bin/map
+fi
 cd ../Companion/bin
 ./companion -hostname $FRM_HOST -port $FRM_PORT &
 PID="$!"
