45e3275705d0c27171b362cb0abdd7abcca2bef5
Jeff Wong <awole20@gmail.com>
2024-10-21 11:38:11 -0700

    hardcode input and output paths

--- alertmanager-config/init.sh
+++ alertmanager-config/init.sh
@@ -2,4 +2,4 @@
 
 test ! -e /usr/local/bin/app && /usr/local/go/bin/go build -o /usr/local/bin/app ./...
 
-/usr/local/bin/app
+OUTPUT_PATH=/etc/alertmanager/alertmanager.yml INPUT_PATH=/usr/src/templates/alertmanager.yml.tmpl /usr/local/bin/app
