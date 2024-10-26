0062222b7d5c1c3da530201d988c0b2c9010f3cd
Jeff Wong <awole20@gmail.com>
2024-10-26 00:51:04 -0700

    fix migration

--- frmcache/src/db/006_rename_save_to_session.sql
+++ frmcache/src/db/006_rename_save_to_session.sql
@@ -1,12 +1,12 @@
 -- Write your migrate up statements here
 
-ALTER TABLE cache RENAME COLUMN save session_name;
-ALTER TABLE cache_with_history RENAME COLUMN save session_name;
+ALTER TABLE cache RENAME COLUMN save TO session_name;
+ALTER TABLE cache_with_history RENAME COLUMN save TO session_name;
 
 ---- create above / drop below ----
 
 -- Write your migrate down statements here. If this migration is irreversible
 -- Then delete the separator line above.
 
-ALTER TABLE cache RENAME COLUMN session_name save;
-ALTER TABLE cache_with_history RENAME COLUMN session_name save;
+ALTER TABLE cache RENAME COLUMN session_name TO save;
+ALTER TABLE cache_with_history RENAME COLUMN session_name TO save;
