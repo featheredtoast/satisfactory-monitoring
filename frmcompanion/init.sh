0678eda586de5fe751899d91704e6165b1ca5179
Jeff Wong <awole20@gmail.com>
2024-10-26 16:02:30 -0700

    reset to using monitor-multiple

--- frmcompanion/init.sh
+++ frmcompanion/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-test ! -e Companion && /usr/bin/git clone --depth 1 -b unregister-old-metrics https://github.com/featheredtoast/FicsitRemoteMonitoringCompanion.git .
+test ! -e Companion && /usr/bin/git clone --depth 1 -b monitor-multiple https://github.com/featheredtoast/FicsitRemoteMonitoringCompanion.git .
 cd Companion
 mkdir -p ./bin/map
 test ! -e ./bin/companion && /usr/local/go/bin/go build -o bin/companion main.go
